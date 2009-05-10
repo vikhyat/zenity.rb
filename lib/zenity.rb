9c890ab405cccbfad65400c340b9cf8483abccfa
vikhyat <vikhyatk@gmail.com>
2009-05-10 12:18:44 +0530

    lib/zenity.rb: Added some comments, modified some code.

--- lib/zenity.rb
+++ lib/zenity.rb
@@ -3,33 +3,30 @@ module Zenity
 
     # Let's just use method_missing instead of defining all of those.
     def method_missing(m, options=false)
-      if [
-        :calendar,
-        :entry,
-        :error,
-        :'file-selection',
-        :info,
-        :list,
-        :notification,
-        :progress,
-        :question,
-        :'text-info',
-        :warning,
-        :scale
+      # Check if the dialog box we have to show is allowed
+      if [ :calendar, :entry, :error, :'file-selection', :info, :list,
+           :notification, :progress, :question, :'text-info', :warning, :scale 
       ].include? m
+        # It's in the array and everything seems to be fine
         ret = %x[ #{parseopts('zenity --' + m.to_s, options)} ]
       else
+        # We don't know about this dialog, raise an error
         raise "Unknown dialog #{m}"
       end
+      # Check if any of the options passed were invalid.
       if ret.rstrip == 'This option is not available. Please see --help for all possible usages.'
-        raise "Invalid option"
+        # Write a message to STDERR if there were invalid options, but don't
+        # raise an error.
+        STDERR << "Invalid option"
       else
+        # Everything looks fine. Return what we got.
         return ret
       end
     end
 
 
     private
+    # Very confusing code that generates the shell command.
     def parseopts(cmd, options)
       if options != false
         options.each do |k, v|
